66aae59c7b673e945bddf6535b801dd24c98e968
Janon-Emersion-T <janonemersion@hotmail.com>
2025-01-08 05:58:46 +0530

    echo "# medicon" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Janon-Emersion-T/medicon.git git push -u origin main

---
+++ frontend/src/views/Landing/About.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    props: {",
+    // Define props here",
+        "    },",
+        "    data() {",
+        "      return {",
+        "        // Define data here",
+        "      };",
+        "    },",
+        "    methods: {",
+        "      // Define methods here",
+        "    },",
+        "  };",
+        "</script>",
+        "",
+        "<style scoped>",
+        "  /* Add component styles here */",
+        "</style>"
+      ],
+      "description": "Vue component"
+    }
+  }
